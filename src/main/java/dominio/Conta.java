84077b39719012eff189fdb932b8ac27a8cbc41d
ralphsbaesso <ralph.baesso@fatec.sp.gov.br>
2018-07-22 19:53:59 -0300

    atualizações

--- src/main/java/dominio/Conta.java
+++ src/main/java/dominio/Conta.java
@@ -8,18 +8,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import enuns.ELeitorArquivo;
 
